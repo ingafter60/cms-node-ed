b29994e22870076fcf1c92e2670e97789a6b604d
ing <inyoman_gurnitha@yahoo.com>
2018-12-17 14:08:28 +0700

    Added Login and Register pages

--- app.js
+++ app.js
@@ -10,8 +10,17 @@ app.set('view engine', 'handlebars');
 app.get('/', (req, res) => {
   res.render('home/index');
 });
+
 app.get('/about', (req, res) => {
   res.render('home/about');
+});
+
+app.get('/login', (req, res) => {
+  res.render('home/login');
+});
+
+app.get('/register', (req, res) => {
+  res.render('home/register');
 });
 
 app.listen(4500, () => {
