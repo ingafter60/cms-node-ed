3d59285c189368d0c38fc6c43b2b19e657f8ec22
ing <inyoman_gurnitha@yahoo.com>
2018-12-17 11:05:29 +0700

    Adding Partials and rename home-nav to navbar

--- app.js
+++ app.js
@@ -7,8 +7,11 @@ app.use(express.static(path.join(__dirname, 'public')));
 app.engine('handlebars', exphbs({ defaultLayout: 'home' }));
 app.set('view engine', 'handlebars');
 
-app.get('', (req, res) => {
+app.get('/', (req, res) => {
   res.render('home/index');
+});
+app.get('/about', (req, res) => {
+  res.render('home/about');
 });
 
 app.listen(4500, () => {
