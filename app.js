4b7532eb9be9111c5c332693c7e4a239e9f502ec
ing <inyoman_gurnitha@yahoo.com>
2018-12-18 16:21:45 +0700

    Install mongoose and added db connection

--- app.js
+++ app.js
@@ -3,11 +3,17 @@ const express = require('express');
 const app = express();
 const path = require('path');
 const exphbs = require('express-handlebars');
+const mongoose = require('mongoose');
 
 // Load Routes
 const indexPagesRoute = require('./routes/pages/index.js');
 const indexAdminRoute = require('./routes/admin/index.js');
 const postsAdminRoute = require('./routes/admin/posts.js');
+
+// MongoDb Connection
+mongoose.connect('mongodb://localhost:27017/cms_node_ed', {useMongoClient: true}).then(db => {
+	console.log('Connected to MongoDb');
+}).catch(error => console.log(error + " Error: Could not connect to MongoDb"));
 
 // Static Files and View Engine
 app.use(express.static(path.join(__dirname, 'public')));
